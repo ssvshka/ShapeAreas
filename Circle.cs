01c49ad7f840ccfaf546e855116549902fddd0e5
ssvshka <ssvshka@gmail.com>
2023-03-18 13:51:14 +0300

    Initial commit

---
+++ Circle.cs
@@ -0,0 +1,21 @@
+﻿namespace ShapeAreas.Shapes;
+
+public class Circle : Shape
+{
+    private readonly double _radius;
+
+    public Circle(double radius)
+    {
+        _radius = radius;
+    }
+
+    public override double CalculateArea() 
+    {
+        if (IsShapeValid())
+        {
+            return Math.PI * Math.Pow(_radius, 2);
+        }
+    }
+
+    public override bool IsShapeValid() => _radius >= 0;
+}
---
+++ Triangle.cs
@@ -0,0 +1,37 @@
+﻿namespace ShapeAreas.Shapes;
+
+public class Triangle : Shape
+{
+    private readonly double A;
+    private readonly double B;
+    private readonly double C;
+
+    public Triangle(double a, double b, double c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public override double CalculateArea()
+    {
+        if (IsShapeValid())
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(
+                p * (p - A) * (p - B) * (p - C));
+        }
+        else
+        {
+            throw new Exception();
+        }
+    }
+
+    public override bool IsShapeValid() =>
+        A >= 0 || B >= 0 || C >= 0;
+
+    public bool IsRightAngled() =>
+        C * C == A * A + B * B ||
+        A * A == B * B + C * C ||
+        B * B == A * A + C * C;
+}
